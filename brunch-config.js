6bc23ef637c346e8da22b4e1d47abd7424854b8c
Rasmus Larsson <rasmus.larsson@gmail.com>
2016-06-04 19:06:55 +0200

    Upgraded to latest version of Phoenix and Brunch.

--- brunch-config.js
+++ brunch-config.js
@@ -39,8 +39,6 @@ exports.config = {
   paths: {
     // Dependencies and current project directories to watch
     watched: [
-      "deps/phoenix/web/static",
-      "deps/phoenix_html/web/static",
       "web/static",
       "test/static",
       "web/elm/Eventless.elm"
@@ -70,6 +68,7 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    whitelist: ["phoenix", "phoenix_html"]
   }
 };
--- mix.exs
+++ mix.exs
@@ -28,8 +28,8 @@ defmodule Eventless.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.0.4"},
-     {:phoenix_html, "~> 2.1"},
+    [{:phoenix, "~> 1.1"},
+     {:phoenix_html, "~> 2.3"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"}]
   end
